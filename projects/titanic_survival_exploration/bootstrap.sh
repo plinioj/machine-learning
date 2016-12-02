d85c144065ccb96e3fc76aa69e67703398eddcff
Plinio Santos <plinio.santos@neolog.com.br>
2016-12-02 10:49:33 -0200

    anaconda installed

--- projects/titanic_survival_exploration/bootstrap.sh
+++ projects/titanic_survival_exploration/bootstrap.sh
@@ -1,3 +1,19 @@
 #!/usr/bin/env bash
 
-pip install --user numpy pandas matplotlib jupyter
+apt-get update
+
+
+anaconda=Anaconda2-4.2.0-Linux-x86_64.sh
+cd /vagrant
+if [[ ! -f $anaconda ]]; then
+    wget --quiet http://repo.continuum.io/archive/$anaconda
+fi
+chmod +x $anaconda
+./$anaconda -b -p /opt/anaconda
+
+cat >> /home/vagrant/.bashrc << END
+# add for anaconda install
+PATH=/opt/anaconda/bin:\$PATH
+END
+
+apt-get install python-pip
